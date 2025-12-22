694163940cc3ba67f4865c3ff8f66f0a3c71c9a3
Hackerl <490021209@qq.com>
2025-12-22 22:22:26 +0800

    feat: asyncio::timeout supports exceptions

--- include/asyncio/time.h
+++ include/asyncio/time.h
@@ -38,6 +38,58 @@ namespace asyncio {
 
         co_return std::expected<std::expected<T, E>, TimeoutError>{std::move(result)};
     }
+
+    template<typename T, typename E>
+        requires std::is_same_v<E, std::exception_ptr>
+    task::Task<T> timeout(task::Task<T, E> task, const std::chrono::milliseconds ms) {
+        if (ms == std::chrono::milliseconds::zero())
+            co_return co_await task;
+
+        auto timer = sleep(ms)
+            .andThen([&] {
+                return task.cancel();
+            });
+
+        std::optional<std::expected<T, std::exception_ptr>> result;
+
+        try {
+            if constexpr (std::is_void_v<T>) {
+                co_await task;
+                result.emplace();
+            }
+            else {
+                result.emplace(co_await task);
+            }
+        }
+        catch (const std::exception &) {
+            result.emplace(std::unexpected{std::current_exception()});
+        }
+
+        if (timer.done()) {
+            if (!timer.future().result()) {
+                if (!*result)
+                    std::rethrow_exception(result->error());
+
+                if constexpr (std::is_void_v<T>)
+                    co_return;
+                else
+                    co_return *std::move(*result);
+            }
+
+            throw zero::error::SystemError{make_error_code(TimeoutError::ELAPSED)};
+        }
+
+        std::ignore = timer.cancel();
+        std::ignore = co_await timer;
+
+        if (!*result)
+            std::rethrow_exception(result->error());
+
+        if constexpr (std::is_void_v<T>)
+            co_return;
+        else
+            co_return *std::move(*result);
+    }
 }
 
 Z_DECLARE_ERROR_CODE(asyncio::TimeoutError)
--- test/time.cpp
+++ test/time.cpp
@@ -1,5 +1,6 @@
 #include "catch_extensions.h"
 #include <asyncio/time.h>
+#include <catch2/matchers/catch_matchers_all.hpp>
 
 ASYNC_TEST_CASE("sleep", "[time]") {
     using namespace std::chrono_literals;
@@ -8,7 +9,7 @@ ASYNC_TEST_CASE("sleep", "[time]") {
     REQUIRE(std::chrono::system_clock::now() - tp > 45ms);
 }
 
-ASYNC_TEST_CASE("timeout", "[time]") {
+ASYNC_TEST_CASE("timeout - error", "[time]") {
     using namespace std::chrono_literals;
 
     SECTION("not expired") {
@@ -50,5 +51,75 @@ ASYNC_TEST_CASE("timeout", "[time]") {
         REQUIRE(result);
         REQUIRE_FALSE(*result);
         REQUIRE(result->error() == std::errc::operation_canceled);
+    }
+}
+
+ASYNC_TEST_CASE("timeout - exception", "[time]") {
+    using namespace std::chrono_literals;
+
+    SECTION("not expired") {
+        REQUIRE_NOTHROW(
+            co_await asyncio::timeout(
+                asyncio::task::spawn([]() -> asyncio::task::Task<void> {
+                    zero::error::guard(co_await asyncio::sleep(10ms));
+                }),
+                20ms
+            )
+        );
+    }
+
+    SECTION("expired") {
+        REQUIRE_THROWS_MATCHES(
+            co_await asyncio::timeout(
+                asyncio::task::spawn([]() -> asyncio::task::Task<void> {
+                    zero::error::guard(co_await asyncio::sleep(20ms));
+                }),
+                10ms
+            ),
+            std::system_error,
+            Catch::Matchers::Predicate<std::system_error>([](const auto &error) {
+                return error.code() == asyncio::TimeoutError::ELAPSED;
+            })
+        );
+    }
+
+    SECTION("expired but cannot be cancelled") {
+        asyncio::Promise<void, std::error_code> promise;
+
+        auto task = asyncio::timeout(
+            asyncio::task::spawn([&]() -> asyncio::task::Task<void> {
+                zero::error::guard(co_await from(asyncio::task::CancellableFuture{
+                    promise.getFuture(),
+                    []() -> std::expected<void, std::error_code> {
+                        return std::unexpected{asyncio::task::Error::CANCELLATION_TOO_LATE};
+                    }
+                }));
+            }),
+            10ms
+        );
+
+        REQUIRE(co_await asyncio::sleep(20ms));
+        REQUIRE_FALSE(task.done());
+
+        promise.resolve();
+        REQUIRE_NOTHROW(co_await task);
+    }
+
+    SECTION("cancel") {
+        auto task = asyncio::timeout(
+            asyncio::task::spawn([]() -> asyncio::task::Task<void> {
+                zero::error::guard(co_await asyncio::sleep(20ms));
+            }),
+            10ms
+        );
+        REQUIRE(task.cancel());
+
+        REQUIRE_THROWS_MATCHES(
+            co_await task,
+            std::system_error,
+            Catch::Matchers::Predicate<std::system_error>([](const auto &error) {
+                return error.code() == std::errc::operation_canceled;
+            })
+        );
     }
 }
